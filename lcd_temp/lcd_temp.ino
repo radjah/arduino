dedb656b0d6dd1895d9e004ba84ed0f2e69157ad
Pavel Frolov <tigrashik@gmail.com>
2014-08-05 17:02:33 +0400

    lcd_temp: С помощью какой-то магии заставил работать

--- lcd_temp/lcd_temp.ino
+++ lcd_temp/lcd_temp.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
@@ -49,12 +50,15 @@ void loop() {
   // Курсор на первый символ
   lcd.home();
   // Переменные
+  int i; // Счетчик
+  // Serial
   String RecvStr="";
   String Eps="";
   String Tmp="";
-  char sTmp[20]="";
-  char TmpOut[5]="";
-  float DigTmp=0.0;
+  // swSerial
+  String swRecvStr="";
+  String swEps="";
+  String swTmp="";
   // Чтение из порта
   if (Serial.available() > 0) {
     char RecvChar = Serial.read();
@@ -64,10 +68,9 @@ void loop() {
     // Проверка на начало последовательности
     if (RecvChar == '=') {
       // Вся ли строка в буфере?
-      // TODO: исправить на реальную длину без первого символа.
       if (Serial.available() >= 14) {
         // Если вся, то читаем всю.
-        for (int i=0;i<14;i++) {
+        for (i=0;i<14;i++) {
           RecvStr += char(Serial.read());
         }
         // Выводим последовательность
@@ -76,38 +79,71 @@ void loop() {
         Eps=RecvStr.substring(5,7);
         // Выделяем и выводим тепературу
         Tmp=RecvStr.substring(7,11);
-        switch (Tmp[0]){
-          case '-':
-          // Отрицательные значения
-            if (Tmp[1]=='F'|Tmp[1]==' '){
-              Tmp[1]='0';
-            }
-            DigTmp=float(Tmp.toInt())/10;
-            dtostrf(DigTmp, 5, 1, TmpOut);
-            lcd.setCursor(3,0);
-            lcd.print(TmpOut);
-//          lcd.setCursor(8,0);
-//          lcd.print("C");
-            break;
-          case '>':
-          // Превышение диапазона
-            lcd.setCursor(3,0);
-            lcd.print(">>>>>");
-            break;
-          case '<':
-          // Ниже диапазона
-            lcd.setCursor(3,0);
-            lcd.print("<<<<<");
-            break;
-          default:
-          // Положительные в диапазоне
-            lcd.setCursor(3,0);
-            lcd.print(" ");
-            lcd.print(Tmp);
+        PrintResult(Tmp, 0, 3);
+      }
+    }
+  }
+  // Чтение из swSeial
+  if (swSerial.available() > 0) {
+    char RecvChar = swSerial.read();
+    // Отладочный вывод
+    swSerial.println(swSerial.available());
+    swSerial.println(RecvChar);
+    // Проверка на начало последовательности
+    if (RecvChar == '=') {
+      // Вся ли строка в буфере?
+      if (swSerial.available() >= 14) {
+        // Если вся, то читаем всю.
+        for (i=0;i<14;i++) {
+          swRecvStr += char(swSerial.read());
         }
+        // Выводим последовательность
+        RecvStr.trim();
+        // Выделяем и выводим e
+        swEps=swRecvStr.substring(5,7);
+        // Выделяем и выводим тепературу
+        swTmp=swRecvStr.substring(7,11);
+        PrintResult(swTmp, 0, 13);        
       }
     }
   }
   // Задержка между замерами
-  delay(1000);
+  delay(100);
+}
+
+// Вывод на экран
+void PrintResult(String Input, int lcdLine, int lcdPos){
+  float DigTmp;
+  char  TmpOut[6]="";
+  swSerial.println(Input);
+  swSerial.println(sizeof(Input));
+  switch (Input[0]){
+    case '-':
+    // Отрицательные значения
+      if (Input[1]=='F'){
+        Input[1]='0';
+      }
+      DigTmp=float(Input.toInt())/10;
+      swSerial.println(Input);
+      dtostrf(DigTmp, 5, 1, TmpOut);
+      swSerial.println(TmpOut);
+      lcd.setCursor(lcdPos,lcdLine);
+      lcd.print(TmpOut);
+      break;
+    case '>':
+    // Превышение диапазона
+      lcd.setCursor(lcdPos,lcdLine);
+      lcd.print(">>>>>");
+      break;
+    case '<':
+    // Ниже диапазона
+      lcd.setCursor(lcdPos,lcdLine);
+      lcd.print("<<<<<");
+      break;
+    default:
+    // Положительные в диапазоне
+      lcd.setCursor(lcdPos,lcdLine);
+      lcd.print(" ");
+      lcd.print(Input);
+  }
 }
