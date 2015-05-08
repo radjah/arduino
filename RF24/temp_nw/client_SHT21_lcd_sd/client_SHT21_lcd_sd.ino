0b4bcf276fe49811a9665281087744497bae5f17
Pavel Frolov <tigrashik@gmail.com>
2015-05-08 10:36:12 +0300

    client_SHT21_nolcd -> client_SHT21_lcd_sd Логгер с дисплеем.

--- RF24/temp_nw/client_SHT21_lcd_sd/client_SHT21_lcd_sd.ino
+++ RF24/temp_nw/client_SHT21_lcd_sd/client_SHT21_lcd_sd.ino
@@ -6,8 +6,23 @@
 #include <RF24.h>
 #include "printf.h"
 #include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
 #include <SD.h>
+
+// Порты LCD
+#define LCD_I2C_ADDR    0x27 // Define I2C Address where the PCF8574T is
+#define BACKLIGHT     3
+#define LCD_EN  2
+#define LCD_RW  1
+#define LCD_RS  0
+#define LCD_D4  4
+#define LCD_D5  5
+#define LCD_D6  6
+#define LCD_D7  7
+
+LiquidCrystal_I2C       lcd(LCD_I2C_ADDR, LCD_EN, LCD_RW, LCD_RS, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 // Пины радио
 #if  defined (__AVR_ATmega2560__) || defined (__AVR_ATmega2561__)
@@ -82,6 +97,18 @@ void setup() {
   radio.startListening();
   radio.printDetails();
   Serial.println("This is CLIENT");
+  // Включаем дисплей
+  lcd.begin(20, 4);
+  lcd.setBacklightPin(BACKLIGHT, POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.clear();
+  lcd.home();
+  lcd.setCursor(0, 1);
+  lcd.print("T('C):");
+  lcd.setCursor(0, 2);
+  lcd.print("H (%):");
+  lcd.setCursor(0, 3);
+  lcd.print("P(Hg):");
 }
 
 void loop() {
@@ -94,13 +121,14 @@ void loop() {
     {
       done = radio.read( &st, sizeof(sendtemp) );
       DateTime MSK (st.dt);
-      char dt[20] = "";
-      sprintf(dt, "%04d/%02d/%02d %02d:%02d:%02d", MSK.year(),
+      char dt[21] = "";
+      sprintf(dt, "%02d/%02d/%02d  %02d:%02d:%02d", MSK.year(),
               MSK.month(),
               MSK.day(),
               MSK.hour(),
               MSK.minute(),
               MSK.second());
+      // Вывод в порт
       Serial.println("Recived!");
       Serial.print("Time: ");
       Serial.println(dt);
@@ -112,6 +140,32 @@ void loop() {
       Serial.println(st.pres);
       Serial.print("Inhum: ");
       Serial.println(st.hum);
+      // Вывод на дисплей
+      lcd.setCursor(0, 0);
+      lcd.print(dt);
+      // Под температуру
+      char tmpc[7] = "";
+      // Под всё остальное
+      char prnt[15] = "";
+      // Вывод температуры внутри и снаружи.
+      lcd.setCursor(7, 1);
+      dtostrf(st.intemp, 6, 2, tmpc);
+      lcd.print(tmpc);
+      lcd.print("/");
+      if (st.outtemp == -127.0) {
+        lcd.print("ERROR!");
+      } else {
+        dtostrf(st.outtemp, 6, 2, tmpc);
+        lcd.print(tmpc);
+      }
+      // Вывод относительной влажности
+      lcd.setCursor(6, 2);
+      dtostrf(st.hum, 14, 2, prnt);
+      lcd.print(prnt);
+      // Вывод давления
+      lcd.setCursor(6, 3);
+      dtostrf(st.pres, 14, 2, prnt);
+      lcd.print(prnt);
       // Запись на карту памяти
       if (isSD == true) {
         Serial.println("Writing...");
